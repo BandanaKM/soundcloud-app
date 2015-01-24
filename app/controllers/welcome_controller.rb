1e5864ba7127a788a9ca0db0336e4cde9908a752
Cheverda <mike.cheverda@gmail.com>
2015-01-24 19:04:01 +0200

    Displaying iframe from soundcloud client response

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
 
   def home
   	@songs = Song.all
+
+    client = SoundCloud.new(client_id: "3d2ec585a5565ddf5124e3ce9f39e2f6")
+    @tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
   end
 
 end
